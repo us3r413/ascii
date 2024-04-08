45e82761b4e0071c7cbe5b5bbd52ced27223ead2
Someone <139086934+us3r413@users.noreply.github.com>
2024-04-08 17:43:22 +0800

    Add files via upload
    QOL change

---
+++ currentfile.py
@@ -0,0 +1,9 @@
+import cv2
+filename = "hiasobi-miku"
+video = cv2.VideoCapture(fr".\{filename}\{filename}.mov")
+def file ():
+    return filename
+def fps ():
+    return video.get(cv2.CAP_PROP_FPS)
+def frame ():
+    return int(video.get(cv2.CAP_PROP_FRAME_COUNT))
