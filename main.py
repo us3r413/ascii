660833b19a6358c9115fca7e85741d6af1b32d7a
Someone <139086934+us3r413@users.noreply.github.com>
2024-04-08 17:42:16 +0800

    Update main.py
    changes to fix latency issue

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import cv2
 import pygame
 import curses
 import time
+import currentfile as cf
+filename = cf.file()
+fps = cf.fps()
+frame = cf.frame()
+pygame.init()
+pygame.mixer.init()
 def main(stdscr):
-    vidcap = cv2.VideoCapture(r"badapple.mov")
-    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    global fps,frame,filename
     spf = 1.0/fps
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(r"badapplemusic.wav")
-    for i in range(0,int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))):
-        start_time = time.time()
-        f = open(fr".\frames\frame_{i}.txt","r")
+    pygame.mixer.music.load(fr".\{filename}\{filename}.wav")
+    i=0
+    start_time=time.time()
+    pygame.mixer.music.play()
+    while(1):
+        duration=time.time()-start_time
+        f = open(fr".\frames\frame_{int(duration/spf)}.txt","r")
         output=f.read()
         f.close()
         stdscr.clear()
-        if (i==0):
-            pygame.mixer.music.play()
         stdscr.addstr(output)
         stdscr.refresh()
-        k = spf+start_time
-        delay_time = max(k - time.time(), 0)
-        time.sleep(delay_time)
     pygame.mixer.music.unload()
-
-curses.wrapper(main)
+if __name__ == "__main__":
+    curses.wrapper(main)
